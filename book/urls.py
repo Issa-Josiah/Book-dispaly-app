244449bc96de0db4374a021ba511da5467d6d9de
David Amedi <amukoahdavid@gmail.com>
2025-12-01 13:52:12 +0300

    creates base and dashboard template files

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,17 @@
 from django.urls import path
+from . import views
+
+app_name = 'book'
 
 urlpatterns = [
+    # dashboard
+    path('', views.dashboard, name='dashboard'),
 
+    # Books
+
+    # Authors
+
+    # Categories
+
+    # Borrow Records
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, redirect
+from django.contrib import messages
+from .models import Book, Author, Category, BorrowRecord
+from .forms import BookForm, AuthorForm, CategoryForm, BorrowForm, ReturnForm
 
 
 # Create your views here.
 
+
+def dashboard(request):
+    # Dashboard view that will show all the liblary statistics
+    return render(request, 'book/dashboard.html')
