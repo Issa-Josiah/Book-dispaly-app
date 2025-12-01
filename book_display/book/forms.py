1e0c741ed5631ce1dfca72c4c4b1a81273f7898b
David Amedi <amukoahdavid@gmail.com>
2025-12-01 11:03:46 +0300

    creates the category form

--- book_display/book/forms.py
+++ book_display/book/forms.py
@@ -28,3 +28,25 @@ class AuthorForm(forms.ModelForm):
             'bio': 'Biography',
             'birth_date': 'Birth Date',
         }
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter category name'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Enter category description'
+            }),
+        }
+        labels = {
+            'name': 'Category Name',
+            'description': 'Description',
+        }
+
+#book form
